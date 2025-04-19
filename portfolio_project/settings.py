8cc4636568194f6a4d6413b857825e51cd95858b
Rama2451655 <113767988+Rama2451655@users.noreply.github.com>
2025-04-19 16:17:12 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ynwd_9$5+5$h&7n1v1tkqoj=xojlor#uvmf3nz%1q!kd65%lr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
