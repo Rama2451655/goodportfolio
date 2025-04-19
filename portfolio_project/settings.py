ddd0aee5b97e1c64a274337678eea5be61e15ead
Rama2451655 <113767988+Rama2451655@users.noreply.github.com>
2025-04-19 17:24:52 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
